8ae431ec95f4d82361b3b23dfd7f291d1d193059
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-29 11:25:40 +0300

    add some files in case we add support for linux later

--- src-tauri/src/minimizer/mod.rs
+++ src-tauri/src/minimizer/mod.rs
@@ -1,8 +1,12 @@
+#[cfg(target_os = "linux")]
+mod linux;
 #[cfg(target_os = "macos")]
 mod macos;
 #[cfg(target_os = "windows")]
 mod windows;
 
+#[cfg(target_os = "linux")]
+use crate::minimizer::linux as sys;
 #[cfg(target_os = "macos")]
 use crate::minimizer::macos as sys;
 #[cfg(target_os = "windows")]
