1416d039b012215d7cc02edc75cb64d467043951
Revxrsal <aloosh.kasasbeh@gmail.com>
2023-09-16 12:28:25 +0300

    silently catch errors as we don't care about them?

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -2,20 +2,23 @@ import "../../styles/index.css";
 import Button from "@src/components/button/Button";
 import Column from "@src/components/layout/Column";
 import Spacer from "@src/components/decoration/Spacer";
-import {getCurrentTab, sendMessageToBackground} from "@src/utils/extension-utils";
-import {commandAddWebsite} from "@src/utils/commands";
+import { getCurrentTab, sendMessageToBackground } from "@src/utils/extension-utils";
+import { commandAddWebsite } from "@src/utils/commands";
 
 export default function Popup() {
     async function addWebsite() {
-        let tab = await getCurrentTab();
-        await sendMessageToBackground(commandAddWebsite(tab.url))
+        try {
+            let tab = await getCurrentTab();
+            await sendMessageToBackground(commandAddWebsite(tab.url));
+        } catch (e) {
+        }
     }
 
     return (
         <main class={"w-full h-full mx-auto my-auto"}>
             <Column class={"center mx-auto my-auto"}>
                 <h1 class={"font-bold text-3xl text text-center"}>Focus Assistant</h1>
-                <Spacer class={"my-2"}/>
+                <Spacer class={"my-2"} />
                 <Button class={"w-40 m-4"} onClick={addWebsite}>
                     Whitelist website
                 </Button>
