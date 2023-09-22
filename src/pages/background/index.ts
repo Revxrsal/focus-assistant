bce8b43865b0589dfb02a6d27df3e3aa53579683
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-09-22 19:36:29 +0300

    don't retry since that would cause endless looping if the endpoint is offline

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -13,15 +13,6 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     if (changeInfo.url || changeInfo.status === "loading") {
         tab = await chrome.tabs.get(tabId);
         let canAccess = await canAccessWebsite(tab.url)
-        // await sendMessageToTab(tabId, commandAccess(canAccess))
-        await keepTrying(canAccess, tabId)
+        await sendMessageToTab(tabId, commandAccess(canAccess))
     }
 })
-
-async function keepTrying(canAccess: boolean, tabId: number) {
-    try {
-        const a = await sendMessageToTab(tabId, commandAccess(canAccess));
-    } catch (e) {
-        await keepTrying(canAccess, tabId)
-    }
-}
